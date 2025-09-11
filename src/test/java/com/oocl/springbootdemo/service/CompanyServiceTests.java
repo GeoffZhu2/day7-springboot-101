518a6660fdc1e51f2ed32be14087c551070de06b
zhuhao <zhuhao4396@foxmail.com>
2025-09-11 20:18:13 +0800

    feat: company service test

---
+++ src/test/java/com/oocl/springbootdemo/service/CompanyServiceTests.java
@@ -0,0 +1,41 @@
+package com.oocl.springbootdemo.service;
+
+import com.oocl.springbootdemo.Company;
+import com.oocl.springbootdemo.exception.CompanyNotFoundException;
+import com.oocl.springbootdemo.repository.CompanyRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class CompanyServiceTests {
+    @Mock
+    private CompanyRepository companyRepository;
+
+    @InjectMocks
+    private CompanyService companyService;
+    @Test
+    public void should_return_company_given_exist_company_id() {
+        Company company = new Company(1, "Apple");
+
+        when(companyRepository.findById(1)).thenReturn(company);
+        Company foundEmployee = companyService.getCompanyById(1);
+        assertEquals(company, foundEmployee);
+        verify(companyRepository, times(1)).findById(1);
+    }
+
+    @Test
+    public void should_return_null_given_not_exist_company_id() {
+        Company company = new Company(1, "Apple");
+
+        when(companyRepository.findById(1)).thenReturn(company);
+        assertThrows(CompanyNotFoundException.class, () -> companyService.getCompanyById(9999));
+        verify(companyRepository, times(1)).findById(9999);
+    }
+
+}
