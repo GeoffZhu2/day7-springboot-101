6865e69d28a98eb797b755ee1ebdca55641684df
zhuhao <zhuhao4396@foxmail.com>
2025-09-11 20:51:02 +0800

    feat: add Company Dto and Employee Dto

---
+++ src/main/java/com/oocl/springbootdemo/dto/EmployeeDto.java
@@ -0,0 +1,17 @@
+package com.oocl.springbootdemo.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDto {
+    private long id;
+    private String name;
+    private String gender;
+    private int age;
+    private double salary;
+}
